40bf1708364b141bb2d9d25e6437f692df1adbb3
Camila <camiladornas@gmail.com>
2024-02-27 10:01:38 -0300

    ♻️ move expense constructor file

---
+++ src/models/expenseConstructor.ts
@@ -0,0 +1,20 @@
+import { randomUUID } from "crypto";
+import { RequestCreateExpense } from "../validate/interfaces";
+
+export default class Expense {
+  Id: string;
+  Description: string;
+  Date: string;
+  User_id: string;
+  Value: string;
+  Created_at: Date;
+
+  constructor(data: RequestCreateExpense) {
+    this.Id = randomUUID();
+    this.Description = data.Description
+    this.Date = data.Date
+    this.User_id = data.User_id
+    this.Value = data.Value
+    this.Created_at = new Date();
+  }
+}
